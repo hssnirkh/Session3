413ea44b887654b0275d6af2c776fd8d74cfdf64
hssnirkh <hassan.irankhah@gmail.com>
2022-10-02 00:32:30 +0330

    Sleepy

--- 15.py
+++ 15.py
@@ -1,3 +1,15 @@
-a = int(input("a = ")
-b = int(input("b = ")
-c = int(input("c = ")
+import math
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+delta = math.pow(b,2)-(4*a*c)
+print(delta)
+if (delta > 0):
+	x1 = (-b + math.sqrt(delta))/(2 * a)
+	x2 = (-b - math.sqrt(delta))/(2 * a)
+	print(x1,x2)
+elif (delta == 0):
+	x = b / 2 * a
+	print(x)
+else:
+	print("not Found")
