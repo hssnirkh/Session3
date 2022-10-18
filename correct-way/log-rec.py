e26ba3cf0e1ee8fbed742c7b3bfb52fca14f5a2e
hssnirkh <hassan.irankhah@gmail.com>
2022-10-18 03:40:40 +0330

    log

---
+++ correct-way/log-rec.py
@@ -0,0 +1,12 @@
+num = int(input("adad : "))
+paye = int(input("paye : "))
+
+def log_rec(n,p):
+	r = n / p
+	s = 1
+	if(r == 1):
+		return 1
+	else:
+		return s + log_rec(r,p)
+print(log_rec(num,paye))
+
